d3bb1f667d313fe0e0f2d8f60a945c1dbb627c2e
Karthik4Moorthy <147975574+Karthik4Moorthy@users.noreply.github.com>
2023-10-15 14:59:18 +0530

    Add files via upload
    Day1Assignments

---
+++ HomeAssignment1.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class HomeAssignment1 {
+
+	public static void main(String[] args) {
+
+float browserVersion =43.2f;
+String browserName="Safari";
+boolean pageloaded=true;
+int noOfBrowser='1';
+char browserLogo='S';
+
+System.out.println("BrowserVersion"+" "+browserVersion);
+System.out.println("browserName"+" "+ browserName);
+System.out.println("pageloaded"+" "+ pageloaded);
+System.out.println("noOfBrowser"+" "+ noOfBrowser);
+System.out.println("browserLogo"+" "+ browserLogo);
+	}
+	
+	
+	
+
+}
---
+++ HomeAssignment2.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class HomeAssignment2 {
+
+	public static void main(String[] args) {
+		int i=45;
+		if (i>0)
+		{
+			System.out.println("The number is positive");
+		}
+		else 
+		{
+			System.out.println("The number is negative");	
+		}
+
+	}
+
+}
